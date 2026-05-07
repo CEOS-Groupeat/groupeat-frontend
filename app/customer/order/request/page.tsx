f5b558d63dde17dfbec5f7f2d4d30eb207f74d84
YJ <hghk0046@gmail.com>
2026-05-07 17:12:13 +0900

    Pakage: svgr 설치

--- app/customer/order/request/page.tsx
+++ app/customer/order/request/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 {
   /* 주문 요청 페이지입니다. */
 }
@@ -5,6 +7,11 @@ export default function CustomerOrderRequestPage() {
   return (
     <div className="">
       <h1>4.0.0 주문 요청 페이지</h1>
+      {/* 해당 페이지에서는 주문 요청을 위한 폼이 존재하며, 주문 요청 완료 시 주문 상세 페이지로 이동. 이때 결제는 카카오페이 및 토스페이로 진행 */}
+      <Link href="/kakao-pay">카카오페이로 결제하기</Link>
+      <Link href="/toss-pay">토스페이로 결제하기</Link>
+
+      {/* 결제 완료되면 어디로 이동하나요? */}
     </div>
   );
 }
--- app/customer/search/page.tsx
+++ app/customer/search/page.tsx
@@ -7,6 +7,7 @@ export default function CustomerSearchPage() {
   return (
     <div className="">
       <h1>2.0.0 검색 결과 페이지</h1>
+      {/* 해당 페이지에서는 드롭다운으로 구현된 필터 기능으로 가게들을 조회하고, 상세 보기를 통해 가게 상세로 이동 */}
       <Link href="/customer/store/detail">상세 보기</Link>
       {/*가게 별 카드에서 상세보기 버튼 */}
     </div>
