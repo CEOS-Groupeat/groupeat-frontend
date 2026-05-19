52373625b0369fcdbe3fdf64b134c3ab3cc67fcb
YJ <hghk0046@gmail.com>
2026-05-19 21:00:31 +0900

    Style: 피그마 아토믹 토큰 등록

--- app/owner/home/page.tsx
+++ app/owner/home/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 {/* 사업자 메인 페이지입니다. */}
 export default function OwnerHomePage() {
   return (
     <div className="">
-      <h1>사업자 홈 페이지</h1>
+      <h1>사업자 홈 페이지: 안녕하세요, 사장님</h1>
+      <Link href='/owner/calendar'>캘린더</Link>
+      <Link href='/owner/orders'>주문 요약</Link>
     </div>
   );
 }
