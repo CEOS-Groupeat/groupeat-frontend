62da9ff4cae1d3b722e827d7344e4a95691983da
a-00-a <sy.lee.2391@gmail.com>
2026-06-01 01:54:04 +0900

    Refactor: StoreCard 피그마 스펙 적용, 잘못된 URL placeholder 추가 및 위치 코드 TODO 주석 추가

--- components/features/StoreCard.tsx
+++ components/features/StoreCard.tsx
@@ -3,7 +3,6 @@ import Star from '@/public/icons/icon_star.svg';
 import Location from '@/public/icons/icon_place.svg';
 import BadgeCategory from '@/components/ui/BadgeCategory';
 import type { Store } from '@/app/customer/search/_types/store.type';
-//공통 타입 위치를 정하기 !
 
 interface StoreCardProps {
   store: Store;
@@ -15,7 +14,7 @@ export default function StoreCard({ store, onClick }: StoreCardProps) {
     <button
       type="button"
       onClick={onClick}
-      className="flex-1 rounded-xl outline outline-1 outline-border-subtle inline-flex flex-col justify-center items-center overflow-hidden text-left"
+      className="w-42 h-[214px] rounded-xl outline outline-1 outline-offset-[-1px] outline-border-subtle flex flex-col overflow-hidden text-left font-['Pretendard']"
     >
       <div className="relative w-full h-24">
         <Image
@@ -25,7 +24,7 @@ export default function StoreCard({ store, onClick }: StoreCardProps) {
             store.imageUrl?.startsWith('http://') ||
             store.imageUrl?.startsWith('https://')
               ? store.imageUrl
-              : `/${store.imageUrl}`
+              : `/images/image_logo.png`
           }
           alt={store.name}
           fill
@@ -33,34 +32,32 @@ export default function StoreCard({ store, onClick }: StoreCardProps) {
         />
       </div>
 
-      <div className="self-stretch bg-background-default p-2.5 flex flex-col gap-2.5">
+      <div className="self-stretch bg-background-default p-2.5 flex flex-col items-center justify-center gap-2.5">
         <div className="flex flex-col gap-2">
           {/* 카테고리 + 이름 + 가격 */}
           <div className="flex flex-col gap-1.5">
             <BadgeCategory label={store.category} />
-            <div className="pl-0.5 flex flex-col">
-              <span className="text-sm font-semibold text-text-default leading-5">
-                {store.name}
-              </span>
-              <span className="text-xs font-medium text-text-default leading-4">
+            <div className="pl-0.5 flex flex-col text-text-default">
+              <span className="text-label1 font-semibold">{store.name}</span>
+              <span className="text-caption1 font-medium">
                 {store.minPrice.toLocaleString()}원 ~{' '}
                 {store.maxPrice.toLocaleString()}원/개
               </span>
             </div>
           </div>
 
-          {/* 별점 + 위치 */}
+          {/* 별점 + 위치 => 위치코드는 추후 수정 예정 */}
           <div className="flex flex-col">
             <div className="flex items-center gap-0.5">
               <Star className="size-3.5" />
-              <span className="text-xs text-text-subtle leading-4">
+              <span className="text-caption2 text-text-subtle">
                 {store.rating}
               </span>
             </div>
             {store.pickupTimeRange && (
               <div className="flex items-center gap-0.5">
                 <Location className="size-3.5" />
-                <span className="text-xs text-text-subtle leading-4">
+                <span className="text-caption2 text-text-subtle">
                   {store.pickupTimeRange}
                 </span>
               </div>
