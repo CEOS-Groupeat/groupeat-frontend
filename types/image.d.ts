7bba0ced515d330ea922eec13972326baaa600cd
YJ <hghk0046@gmail.com>
2026-06-05 16:01:20 +0900

    Fix: Github Actions 에러 워크플로우 전부 수정

---
+++ types/image.d.ts
@@ -0,0 +1,26 @@
+// types/images.d.ts
+
+declare module '*.png' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: import('next/image').StaticImageData;
+  export default content;
+}
--- types/term.ts
+++
@@ -1,9 +0,0 @@
-// 1. API 응답 스펙에 맞춘 타입 정의
-interface Term {
-  termsId: number;
-  title: string;
-  content: string;
-  required: boolean;
-  targetType: string;
-  version: string;
-}
