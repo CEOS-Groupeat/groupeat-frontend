e469ea3332e85e598f89cd2263b1c6802cc200cb
YJ <hghk0046@gmail.com>
2026-06-05 15:41:12 +0900

    Feat: Github Actions 도입

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     siteName: 'Groupeat',
     images: [
       {
-        url: '/images/og-default.jpg',
+        url: '/public/images/OpenGraph.png',
         width: 1200,
         height: 630,
         alt: '그루핏 대표 이미지',
