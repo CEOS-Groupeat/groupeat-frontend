12262c95f64364fa463fd6cc27df4410e197d36f
a-00-a <sy.lee.2391@gmail.com>
2026-06-16 01:46:16 +0900

    Feat: CartStoreHeader 컴포넌트 구현

---
+++ app/customer/cart/_components/CartStoreHeader.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import RadioOn from '@/public/icons/icon_raidoOn.svg';
+import RadioOff from '@/public/icons/icon_raidoOff.svg';
+import ToStoreIcon from '@/public/icons/icon_arrow_right.svg';
+
+interface CartStoreHeaderProps {
+  storeId: number;
+  storeName: string;
+  allSelected: boolean;
+  onSelectAll: () => void;
+}
+
+export default function CartStoreHeader({
+  storeId,
+  storeName,
+  allSelected,
+  onSelectAll,
+}: CartStoreHeaderProps) {
+  const router = useRouter();
+
+  return (
+      <div className="flex gap-1 pt-5">
+        <button type="button" onClick={onSelectAll} className="flex shrink-0">
+          {allSelected ? (
+            <RadioOn className="size-6 text-icon-default" />
+          ) : (
+            <RadioOff className="size-6 text-icon-disable" />
+          )}
+        </button>
+        <span className="text-body font-bold text-text-default font-['Pretendard']">
+          {storeName}
+        </span>
+        <button
+          type="button"
+          onClick={() => router.push(`/customer/store/${storeId}`)}
+          className="flex items-center shrink-0"
+        >
+          <ToStoreIcon className="size-4 text-icon-subtle" />
+        </button>
+      </div>
+  );
+}
