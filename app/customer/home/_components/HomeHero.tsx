f48080a1ecc80761697b2b9a6b24788f4ecfc7b4
a-00-a <sy.lee.2391@gmail.com>
2026-06-15 18:02:19 +0900

    Feat: HomeHero use client로 변경 및 장바구니 버튼 클릭 시 페이지 이동

--- app/customer/home/_components/HomeHero.tsx
+++ app/customer/home/_components/HomeHero.tsx
@@ -1,9 +1,14 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import Logo from '@/public/icons/icon-logo_customer_home.svg';
 import Bell from '@/public/icons/icon-bell.svg';
 import Cart from '@/public/icons/icon-shopping_cart.svg';
 import HomeSearchBar from './HomeSearchBar';
 
 export default function HomeHero() {
+  const router = useRouter();
+
   return (
     <section className="w-full bg-brand-default">
       {/* Header Container */}
@@ -17,7 +22,9 @@ export default function HomeHero() {
 
         <div className="flex items-center gap-2">
           <Bell />
+          <button type="button" onClick={() => router.push('/customer/cart')}>
           <Cart />
+          </button>
         </div>
       </div>
 
