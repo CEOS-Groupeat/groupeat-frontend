86cc6ba35c8f86eca224f4cf9905385b9ad70a5b
a-00-a <sy.lee.2391@gmail.com>
2026-06-18 01:14:56 +0900

    Refactor: ReviewHeader 뒤로가기 버튼 aria-label 추가

--- app/customer/store/review/_components/ReviewHeader.tsx
+++ app/customer/store/review/_components/ReviewHeader.tsx
@@ -9,7 +9,7 @@ export default function ReviewHeader() {
 
   return (
     <div className="flex w-full mt-10 p-4 relative">
-      <button type="button" onClick={() => router.back()}>
+      <button type="button" onClick={() => router.back()} aria-label="뒤로가기">
         <BackIcon className="size-5 text-icon-default" />
       </button>
       <span className="absolute left-1/2 -translate-x-1/2 text-text-default text-headline3 font-semibold font-['Pretendard']">
