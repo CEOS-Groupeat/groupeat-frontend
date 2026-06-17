edb5ccdc69393068735cbfb02f38d36ba2c48b26
a-00-a <sy.lee.2391@gmail.com>
2026-06-17 21:49:13 +0900

    Feat: ReviewHeader 컴포넌트 구현

---
+++ app/customer/store/review/_components/ReviewHeader.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import BackIcon from '@/public/icons/icon_arrow_Left.svg';
+
+export default function ReviewHeader() {
+  const router = useRouter();
+  const storeName = '데이브런치';
+
+  return (
+    <div className="flex w-full mt-10 p-4 relative">
+      <button type="button" onClick={() => router.back()}>
+        <BackIcon className="size-5 text-icon-default" />
+      </button>
+      <span className="absolute left-1/2 -translate-x-1/2 text-text-default text-headline3 font-semibold font-['Pretendard']">
+        {storeName}
+      </span>
+    </div>
+  );
+}
