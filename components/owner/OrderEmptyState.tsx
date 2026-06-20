e5c68ef572a3e2d6710b86aad568e0f247573569
a-00-a <sy.lee.2391@gmail.com>
2026-06-20 23:53:58 +0900

    Feat: OrderEmptyState 컴포넌트 구현

---
+++ components/owner/OrderEmptyState.tsx
@@ -0,0 +1,14 @@
+import EmptyStateImage from '@/public/icons/emptyState-owner-order.svg';
+
+export default function OrderEmptyState() {
+  return (
+    <>
+      <div className="flex-1 flex flex-col justify-center items-center gap-1 mb-[100px]">
+        <EmptyStateImage className="w-[140px] h-[123px]" />
+        <span className="text-label1 text-text-placeholder font-medium font-['Pretendard']">
+          주문 내역이 없습니다.
+        </span>
+      </div>
+    </>
+  );
+}
