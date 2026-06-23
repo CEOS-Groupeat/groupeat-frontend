3e43fd9d6127255d717f1ceb1c43cca200e3b2d6
YJ <hghk0046@gmail.com>
2026-06-23 14:26:21 +0900

    Fix: 미사용 UI 코드 제거

--- app/owner/orders/[orderId]/page.tsx
+++ app/owner/orders/[orderId]/page.tsx
@@ -1,22 +1,24 @@
-import OrderStatusBar from '@/app/customer/order/[orderId]/_components/OrderStatusBar';
 import OrderCard from '@/app/customer/order/request/_components/OrderCard';
 import OrderPrice from '@/app/customer/order/request/_components/OrderPrice';
 import SectionDivider from '@/components/ui/SectionDivider';
 import ArrowLeft from '@/public/icons/icon_arrow_Left.svg';
 import Ellipse from '@/public/icons/icon_ellipse.svg';
-import Alert from '@/public/icons/icon_alert.svg';
+import { useRouter } from 'next/navigation';
 
 export default function OwnerOrderDetail() {
+    
   const cancelOrder = () => {
     // todo: 주문취소
     return;
   };
 
+  const router = useRouter();
+
   return (
     <div className="w-full flex pb-16 flex-col items-center gap-6 bg-white">
       <header className="w-full flex pt-10 items-start gap-2.5 self-stretch">
         <div className="w-full flex p-4 items-center justify-between self-stretch">
-          <ArrowLeft className="w-5 h-5" />
+          <ArrowLeft className="w-5 h-5" onClick={() => router.back()} />
           <h1 className="text-text-default text-headline3 font-semibold">
             주문 상세
           </h1>
