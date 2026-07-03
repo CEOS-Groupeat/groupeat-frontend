8b4bca801b87f4ad69aa450e22b2da964d0cd997
a-00-a <sy.lee.2391@gmail.com>
2026-07-04 02:54:42 +0900

    Feat: FiltersContainer 컴포넌트 구현 (검색창, picker & category 섹션)

---
+++ app/customer/home/_components/FiltersContainer.tsx
@@ -0,0 +1,33 @@
+import HomeSearchBar from './HomeSearchBar';
+import IconDownArrow from '@/public/icons/icon_arrow_down.svg';
+import CategorySection from './CategorySection';
+import PickerSection from './PickerSection';
+
+export default function FiltersContainer() {
+  return (
+    <section className="w-full bg-background-default pt-6 pb-10 px-4 font-['Pretendard']">
+      <h1 className="text-text-subtlest text-body font-medium mb-0.5">
+        단체 주문 어디서부터 시작할까요?
+      </h1>
+      <div className="flex gap-2 mb-2.5">
+        <span className="text-headline1 font-bold text-text-default">지역</span>
+        <IconDownArrow className="size-5 text-icon-subtle my-2" />
+      </div>
+      <HomeSearchBar />
+
+      <div className="flex flex-col gap-2 mt-5">
+        <span className="text-label2 font-semibold text-text-subtlest">
+          조건에 맞는 맞춤 가게 찾기
+        </span>
+        <PickerSection />
+      </div>
+
+      <div className="flex flex-col gap-2 mt-5">
+        <span className="text-label2 font-semibold text-text-subtlest">
+          카테고리별 탐색
+        </span>
+        <CategorySection />
+      </div>
+    </section>
+  );
+}
