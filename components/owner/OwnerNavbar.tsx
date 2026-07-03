a011b89988554bc93eb87fe1022583e1d1a1e238
YJ <hghk0046@gmail.com>
2026-07-03 16:31:25 +0900

    Fix: Owner Navbar UI 변경

--- components/owner/OwnerNavbar.tsx
+++ components/owner/OwnerNavbar.tsx
@@ -3,16 +3,16 @@
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
-import HomeOn from '@/public/icons/icon_home_on.svg';
-import HomeOff from '@/public/icons/icon_cart_off.svg';
-import OrderManageOn from '@/public/icons/icon_home_on.svg';
-import OrderManageOff from '@/public/icons/icon_cart_off.svg';
-import StoreInfoOn from '@/public/icons/icon_home_on.svg';
-import StoreInfoOff from '@/public/icons/icon_cart_off.svg';
-import ReviewManageOn from '@/public/icons/icon_home_on.svg';
-import ReviewManageOff from '@/public/icons/icon_cart_off.svg';
-import MyPageOn from '@/public/icons/icon_home_on.svg';
-import MyPageOff from '@/public/icons/icon_cart_off.svg';
+import HomeOn from '@/public/icons/icon_navicon_home_on.svg';
+import HomeOff from  '@/public/icons/icon_navicon_home_off.svg';
+import OrderManageOn from '@/public/icons/icon_navicon_history_on.svg';
+import OrderManageOff from '@/public/icons/icon_navicon_history_off.svg';
+import StoreInfoOn from '@/public/icons/icon_navicon_store_on.svg';
+import StoreInfoOff from '@/public/icons/icon_navicon_store_off.svg';
+import FavoriteOn from '@/public/icons/icon_navicon_favorite_on.svg';
+import FavoriteOff from '@/public/icons/icon_navicon_favorite_off.svg';
+import MyPageOn from '@/public/icons/icon_navicon_my_on.svg';
+import MyPageOff from '@/public/icons/icon_navicon_my_off.svg';
 
 const navigationItems = [
   { label: '홈', href: '/owner/home', iconOn: HomeOn, iconOff: HomeOff },
@@ -29,10 +29,10 @@ const navigationItems = [
     iconOff: StoreInfoOff,
   },
   {
-    label: '리뷰 관리',
-    href: '/owner/review',
-    iconOn: ReviewManageOn,
-    iconOff: ReviewManageOff,
+    label: '즐겨찾기',
+    href: '/owner/favorites',
+    iconOn: FavoriteOn,
+    iconOff: FavoriteOff,
   },
   {
     label: '계정',
@@ -58,11 +58,9 @@ const NavigationMenu = ({
   return (
     <Link
       href={href}
-      className={`flex flex-col justify-center items-center gap-2 flex-1 h-16 transition-all ${
-        active ? '' : 'pt-4'
-      }`}
+      className='flex flex-col justify-center items-center gap-2 flex-1 h-16 transition-all bg-background-default'
     >
-      <div className="flex items-center justify-center w-5 h-5 shrink-0">
+      <div className="flex items-center justify-center w-6 h-6 shrink-0">
         {active ? (
           <IconOn className="w-full h-full" />
         ) : (
@@ -85,7 +83,7 @@ export default function OwnerNavbar() {
   const pathname = usePathname();
 
   return (
-    <div className="fixed bottom-0 left-0 z-50 w-full flex px-5 pb-4 justify-between items-start bg-background-default shadow-[0px_-3px_12px_0px_rgba(165,179,198,0.10)]">
+    <div className="fixed bottom-0 left-0 z-50 w-full flex pb-1 justify-between items-start bg-background-default shadow-[0px_-3px_12px_0px_rgba(165,179,198,0.10)]">
       <div className="flex items-start justify-between flex-1 w-full">
         {navigationItems.map((item) => (
           <NavigationMenu
