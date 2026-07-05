651ab92be659fb14d1d3b65a5a5fc7535ff07b04
a-00-a <sy.lee.2391@gmail.com>
2026-07-05 21:22:51 +0900

    Style: SearchField 패딩 수정

--- components/ui/SearchField.tsx
+++ components/ui/SearchField.tsx
@@ -22,7 +22,7 @@ export default function SearchField({
   autoFocus,
 }: SearchFieldProps) {
   return (
-    <div className="w-full min-h-11 bg-background-subtle rounded-lg pr-1.5 pl-3 py-1.5 flex justify-between items-center overflow-hidden">
+    <div className="w-full min-h-11 bg-background-subtle rounded-lg px-3 py-1.5 flex justify-between items-center overflow-hidden">
       <div className="flex items-center gap-2 flex-1">
         {iconPosition === 'left' && (
           <Search className="shrink-0 text-icon-subtlest" />
