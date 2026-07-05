1acfa4ead4b8bc59a94a50903173eefb594acc40
a-00-a <sy.lee.2391@gmail.com>
2026-07-06 08:49:18 +0900

    Fix: RecentKeywordChip 접근성 개선 (삭제 버튼 키보드 접근, MouseEvent 타입 import 수정)

--- app/customer/search/recent/_components/RecentKeywordChip.tsx
+++ app/customer/search/recent/_components/RecentKeywordChip.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import type { MouseEvent } from 'react';
 import CloseIcon from '@/public/icons/icon_close.svg';
 
 interface RecentKeywordChipProps {
@@ -19,13 +20,24 @@ export default function RecentKeywordChip({
       onClick={onClick}
       className="shrink-0 pl-2.5 pr-3.5 py-2 bg-background-default rounded-full outline outline-1 outline-offset-[-1px] outline-border-default flex items-center gap-1"
     >
-      <CloseIcon
-        className="size-4 text-icon-subtle shrink-0"
-        onClick={(e: React.MouseEvent) => {
+      <span
+        role="button"
+        tabIndex={0}
+        onClick={(e: MouseEvent) => {
           e.stopPropagation();
           onRemove();
         }}
-      />
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            e.stopPropagation();
+            onRemove();
+          }
+        }}
+        aria-label="최근 검색어 삭제"
+      >
+        <CloseIcon className="size-4 text-icon-subtle shrink-0" />
+      </span>
+
       <span className="text-label2 whitespace-nowrap font-normal text-text-default font-['Pretendard']">
         {keyword}
       </span>
