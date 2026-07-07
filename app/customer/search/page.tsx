aab2b0934a7145febc4fd83318396598a8431eef
a-00-a <sy.lee.2391@gmail.com>
2026-07-07 16:47:18 +0900

    Feat: 정렬드롭다운에 가나다 순 추가

--- app/customer/search/page.tsx
+++ app/customer/search/page.tsx
@@ -35,7 +35,7 @@ function SearchContent() {
   } = useSearchStore();
   const { search, data: keywordData, isLoading } = useSearchStores();
 
-  const [sort, setSort] = useState('DISCOUNT_DESC');
+  const [sort, setSort] = useState('NONE');
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [filterKey, setFilterKey] = useState(0);
   const [searchInput, setSearchInput] = useState(keyword);
