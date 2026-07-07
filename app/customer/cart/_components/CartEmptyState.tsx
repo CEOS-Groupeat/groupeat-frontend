beebda061248e643b2c33f47cd2b07b96a1812ab
a-00-a <sy.lee.2391@gmail.com>
2026-07-07 19:12:00 +0900

    Feat: 담은 메뉴 없을 시 장바구니 페이지 구현

--- app/customer/cart/_components/CartEmptyState.tsx
+++ app/customer/cart/_components/CartEmptyState.tsx
@@ -1,8 +1,10 @@
+import EmptyShoppingCart from '@/public/illust/illust_EmptyShoppingCart.svg';
+
 export default function CartEmptyState() {
   return (
-    <div className="flex flex-col justify-center items-center mt-[138px]">
-      <div className="size-[150px] bg-text-disabled rounded-[15.4px] mb-5" />
-      <span className="text-body text-text-subtle font-medium font-['Pretendard'] mb-3">
+    <div className="flex flex-col justify-center items-center mt-40 px-[82px]">
+      <EmptyShoppingCart aria-hidden="true" />
+      <span className="text-body text-text-subtle font-medium font-['Pretendard'] text-center">
         장바구니에 담은 메뉴가 없습니다
       </span>
     </div>
