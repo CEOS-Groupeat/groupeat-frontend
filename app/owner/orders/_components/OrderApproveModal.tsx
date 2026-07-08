e1c5ca7a627421b7c4dba3cdc88158a5a06a3f1e
a-00-a <sy.lee.2391@gmail.com>
2026-07-08 16:18:25 +0900

    Feat: 주문 승인 확인 모달 추가

---
+++ app/owner/orders/_components/OrderApproveModal.tsx
@@ -0,0 +1,23 @@
+import IllustDialogModal from '@/components/ui/IllustDialogModal';
+import OrderAcceptIllustIcon from '@/public/illust/illust_OrderAccept.svg';
+
+interface OrderApproveModalProps {
+  onClose: () => void;
+  onApprove: () => void;
+}
+
+export default function OrderApproveModal({
+  onClose,
+  onApprove,
+}: OrderApproveModalProps) {
+  return (
+    <IllustDialogModal
+      icon={<OrderAcceptIllustIcon className="shrink-0" />}
+      title="주문을 승인하시겠습니까?"
+      description="픽업 날짜에 맞춰 음식을 준비해 주세요"
+      primaryButton={{ label: '돌아가기', onClick: onClose }}
+      secondaryButton={{ label: '승인하기', onClick: onApprove }}
+      onClose={onClose}
+    />
+  );
+}
