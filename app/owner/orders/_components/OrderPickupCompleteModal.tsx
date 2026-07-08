4d478810b43f364c3b2582d29f69784e88ef0c16
a-00-a <sy.lee.2391@gmail.com>
2026-07-09 02:55:40 +0900

    Feat: 승인/픽업완료 모달에 isLoading prop 추가

--- app/owner/orders/_components/OrderPickupCompleteModal.tsx
+++ app/owner/orders/_components/OrderPickupCompleteModal.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import IllustDialogModal from '@/components/ui/IllustDialogModal';
 import OrderPickupCompleteIllustIcon from '@/public/illust/illust_PickupDone.svg';
@@ -6,19 +6,29 @@ import OrderPickupCompleteIllustIcon from '@/public/illust/illust_PickupDone.svg
 interface OrderPickupCompleteModalProps {
   onClose: () => void;
   onPickupComplete: () => void;
+  isLoading?: boolean;
 }
 
 export default function OrderPickupCompleteModal({
   onClose,
   onPickupComplete,
+  isLoading,
 }: OrderPickupCompleteModalProps) {
   return (
     <IllustDialogModal
       icon={<OrderPickupCompleteIllustIcon className="shrink-0" />}
       title="픽업이 완료되었나요?"
       description="픽업 완료 여부 입력 후에는 수정이 어려워요"
-      primaryButton={{ label: '돌아가기', onClick: onClose }}
-      secondaryButton={{ label: '픽업 완료', onClick: onPickupComplete }}
+      primaryButton={{
+        label: '돌아가기',
+        onClick: onClose,
+        disabled: isLoading,
+      }}
+      secondaryButton={{
+        label: '픽업 완료',
+        onClick: onPickupComplete,
+        disabled: isLoading,
+      }}
       onClose={onClose}
     />
   );
