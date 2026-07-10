ec74c397f49fb03ba5ddf0b135df102d075f30c9
a-00-a <sy.lee.2391@gmail.com>
2026-07-11 01:32:58 +0900

    Feat: 주문 받기 토글, 주문 수량, 최소 주문 기한 컴포넌트 추가

---
+++ app/owner/shop/operations/_components/OrderQuantityFields.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import InputField from '@/components/ui/InputField';
+
+interface OrderQuantityFieldsProps {
+  minQuantity: string;
+  maxQuantity: string;
+  onMinChange: (value: string) => void;
+  onMaxChange: (value: string) => void;
+}
+
+export default function OrderQuantityFields({
+  minQuantity,
+  maxQuantity,
+  onMinChange,
+  onMaxChange,
+}: OrderQuantityFieldsProps) {
+  return (
+    <div className="self-stretch flex flex-col justify-start items-start gap-3 font-['Pretendard']">
+      <span className="text-center text-text-default text-body font-medium">
+        주문 수량
+      </span>
+      <div className="self-stretch flex justify-center items-end gap-3">
+        <InputField
+          label="최소"
+          value={minQuantity}
+          onChange={(e) => onMinChange(e.target.value)}
+          labelClassName="text-label2 font-normal text-text-subtle"
+          className="flex-1"
+          inputClassName="!text-body !font-medium !text-text-default"
+        />
+        <InputField
+          label="최대"
+          value={maxQuantity}
+          onChange={(e) => onMaxChange(e.target.value)}
+          labelClassName="text-label2 font-normal text-text-subtle"
+          className="flex-1"
+          inputClassName="!text-body !font-medium !text-text-default"
+        />
+      </div>
+    </div>
+  );
+}
