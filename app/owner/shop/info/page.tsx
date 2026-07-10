787d0fd465300d1da05d44e402eabf73a90cded2
a-00-a <sy.lee.2391@gmail.com>
2026-07-10 15:41:10 +0900

    Chore: page.tsx 서버 컴포넌트 유지 확인, ShopSection 연결

--- app/owner/shop/info/page.tsx
+++ app/owner/shop/info/page.tsx
@@ -1,5 +1,5 @@
 import ShopHeader from '@/app/owner/shop/_components/ShopHeader';
-import ShopSection from '@/app/owner/shop/_components/ShopSection';
+import ShopSection from '@/app/owner/shop/info/_components/ShopSection';
 import OwnerNavbar from '@/components/owner/OwnerNavbar';
 
 export default function StoreInfoPage() {
