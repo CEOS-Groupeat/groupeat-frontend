759a6369c4371d31669d4a8e757f7f8d1340ab4c
a-00-a <sy.lee.2391@gmail.com>
2026-07-10 10:28:55 +0900

    Feat: 편집 가능한 프로필 필드 컴포넌트 추가

---
+++ components/features/profile/ProfileField.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+interface ProfileFieldProps {
+  label: string;
+  value: string;
+  placeholder: string;
+  onChange: (value: string) => void;
+}
+
+export default function ProfileField({
+  label,
+  value,
+  placeholder,
+  onChange,
+}: ProfileFieldProps) {
+  return (
+    <div className="flex flex-col gap-2 font-['Pretendard']">
+      <span className="text-label1 font-normal text-text-default">{label}</span>
+      <input
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+        className="w-full h-11 pl-4 pr-3 py-3 rounded-lg outline outline-1 outline-offset-[-1px] outline-border-strong text-body font-normal text-text-default placeholder:text-body placeholder:font-normal placeholder:text-text-placeholder"
+      />
+    </div>
+  );
+}
