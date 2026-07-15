02fdaeefe260e6ad25147c9c80943ed4170295a3
a-00-a <sy.lee.2391@gmail.com>
2026-07-15 13:48:14 +0900

    Fix: 최소 주문 가능 기한 설정 컴포넌트 ui 수정

--- app/owner/shop/operations/_components/MinOrderDaysField.tsx
+++ app/owner/shop/operations/_components/MinOrderDaysField.tsx
@@ -10,21 +10,22 @@ export default function MinOrderDaysField({
   onChange,
 }: MinOrderDaysFieldProps) {
   return (
-    <div className="self-stretch flex flex-col justify-start items-start gap-3">
-      <span className="text-center text-text-default text-base font-medium font-['Pretendard'] leading-6">
+    <div className="self-stretch flex flex-col justify-start items-start gap-3 font-['Pretendard'] mb-12">
+      <span className="text-center text-text-default text-body font-medium">
         최소 주문 가능 기한
       </span>
       <div className="self-stretch flex justify-start items-center gap-2">
-        <span className="text-text-default text-sm font-normal font-['Pretendard'] leading-5">
+        <span className="text-text-default text-label1 font-normal">
           픽업일
         </span>
         <input
           type="number"
           value={value}
           onChange={(e) => onChange(e.target.value)}
-          className="size-11 p-3 bg-background-default rounded-lg outline outline-1 outline-offset-[-1px] outline-border-strong text-center text-text-default text-base font-medium font-['Pretendard'] leading-6"
+          placeholder={'0'}
+          className="size-11 p-3 bg-background-default rounded-lg outline outline-1 outline-offset-[-1px] outline-border-strong text-center text-text-default text-body font-medium placeholder:text-text-placeholder placeholder:font-normal placeholder:text-body placeholder:leading-6"
         />
-        <span className="text-text-default text-sm font-normal font-['Pretendard'] leading-5">
+        <span className="text-text-default text-label1 font-normal">
           일 전까지 주문 가능
         </span>
       </div>
