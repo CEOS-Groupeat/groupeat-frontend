803f1429593bbf3d0209259fcb5cd2013001620f
a-00-a <sy.lee.2391@gmail.com>
2026-07-15 13:49:51 +0900

    Fix: 주문 수량 설정 컴포넌트 ui 수정

--- app/owner/shop/operations/_components/OrderQuantityFields.tsx
+++ app/owner/shop/operations/_components/OrderQuantityFields.tsx
@@ -17,25 +17,25 @@ export default function OrderQuantityFields({
 }: OrderQuantityFieldsProps) {
   return (
     <div className="self-stretch flex flex-col justify-start items-start gap-3 font-['Pretendard']">
-      <span className="text-center text-text-default text-body font-medium">
-        주문 수량
-      </span>
+      <span className="text-text-default text-body font-medium">주문 수량</span>
       <div className="self-stretch flex justify-center items-end gap-3">
         <InputField
           label="최소"
           value={minQuantity}
           onChange={(e) => onMinChange(e.target.value)}
           labelClassName="text-label2 font-normal text-text-subtle"
-          className="flex-1"
-          inputClassName="!text-body !font-medium !text-text-default"
+          className="flex-1 !gap-1.5"
+          placeholder="수량"
+          inputClassName="!text-body !font-medium !text-text-default placeholder:!font-normal placeholder:!text-base placeholder:!leading-6"
         />
         <InputField
           label="최대"
           value={maxQuantity}
           onChange={(e) => onMaxChange(e.target.value)}
           labelClassName="text-label2 font-normal text-text-subtle"
-          className="flex-1"
-          inputClassName="!text-body !font-medium !text-text-default"
+          className="flex-1 !gap-1.5"
+          placeholder="수량"
+          inputClassName="!text-body !font-medium !text-text-default placeholder:!font-normal placeholder:!text-base placeholder:!leading-6"
         />
       </div>
     </div>
