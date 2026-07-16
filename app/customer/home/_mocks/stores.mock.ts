34e73cda6681b833ecd574cd7d3225b727023f02
a-00-a <sy.lee.2391@gmail.com>
2026-07-16 17:45:41 +0900

    Chore: 미사용 목데이터 정리

--- app/customer/home/_mocks/stores.mock.ts
+++ app/customer/home/_mocks/stores.mock.ts
@@ -1,39 +1,6 @@
 // MVP 구현범위: 고객 메인 페이지-가게 추천 api 대신 mocks 데이터로 구현
 import { HomeStore } from '../_types/recommendation.type';
 
-export const discountStores: HomeStore[] = [
-  {
-    id: 4,
-    name: '익선잡방',
-    category: '음료',
-    priceRange: '5,000원 ~ 12,000원/개',
-    rating: 4.1,
-    reviewCount: 3,
-    location: '종로구 익선동',
-    image: '/images/ikseon-jabbang.png',
-  },
-  {
-    id: 5,
-    name: '하울즈',
-    category: '샌드위치&김밥',
-    priceRange: '7,000원 ~ 12,000원/개',
-    rating: 4.7,
-    reviewCount: 3,
-    location: '마포구 아현동',
-    image: '/images/howls.png',
-  },
-  {
-    id: 6,
-    name: '데이브런치',
-    category: '샌드위치&김밥',
-    priceRange: '7,000원 ~ 12,000원/개',
-    rating: 4.7,
-    reviewCount: 3,
-    location: '마포구 아현동',
-    image: '/images/day-brunch.png',
-  },
-];
-
 export const recommendedStores: HomeStore[] = [
   {
     id: 7,
