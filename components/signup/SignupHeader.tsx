53d6120dab429279be5ed282867b8c876fc4bce2
a-00-a <sy.lee.2391@gmail.com>
2026-07-18 23:17:35 +0900

    Fix: 공통 회원가입 1단계에서 뒤로가기 시 로그인 페이지로 이동하게 수정

--- components/signup/SignupHeader.tsx
+++ components/signup/SignupHeader.tsx
@@ -1,13 +1,30 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import ArrowLeft from '@/public/icons/icon_arrow_Left.svg';
 import { useSignupStore } from '@/store/useSignupStore';
 
 export default function SignupHeader() {
-  const {prevStep} = useSignupStore();
+  const router = useRouter();
+  const { step, prevStep } = useSignupStore();
+
+  const handleBack = () => {
+    if (step === 1) {
+      router.replace('/login');
+    } else {
+      prevStep();
+    }
+  };
 
   return (
     <div className="w-full flex flex-col pt-10">
       <div className="flex py-4 items-center justify-between self-stretch">
-        <ArrowLeft width={20} height={20} className="cursor-pointer" onClick={prevStep}/>
+        <ArrowLeft
+          width={20}
+          height={20}
+          className="cursor-pointer"
+          onClick={handleBack}
+        />
         <span className="text-headline3 font-bold">회원가입</span>
         <div className="w-5 h-5" />
       </div>
