a5a8607eaf85b54f64c7bc69b7a963c0d0f600f2
a-00-a <sy.lee.2391@gmail.com>
2026-07-19 14:17:29 +0900

    Feat: 사업자 홈 오늘 픽업 건 카드 클릭 시 주문 상세 페이지로 이동

--- components/owner/OrderList.tsx
+++ components/owner/OrderList.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 interface OrderListProps {
   orderId: number | undefined;
   isReorder: boolean;
@@ -17,8 +21,13 @@ export default function OrderList({
   menu,
   quantity,
 }: OrderListProps) {
+  const router = useRouter();
+
   return (
-    <div className="w-full flex p-3 flex-col items-start gap-2 rounded-lg border border-px border-border-subtle bg-static-white shadow-[6px_6px_54px_0_rgba(0, 0, 0, 0.05)]">
+    <div
+      onClick={() => router.push(`/owner/orders/${orderId}`)}
+      className="w-full flex p-3 flex-col items-start gap-2 rounded-lg border border-px border-border-subtle bg-static-white shadow-[6px_6px_54px_0_rgba(0, 0, 0, 0.05)] cursor-pointer"
+    >
       <div className="flex pb-2 flex-col items-end gap-1 self-stretch border-b border-px border-border-default">
         <div className="flex flex-col items-start gap-1.5 self-stretch">
           {isReorder && (
@@ -30,26 +39,32 @@ export default function OrderList({
           )}
           <div className="flex justify-between items-center self-stretch">
             <div className="flex flex-col items-start">
-                <p className="text-text-subtlest text-caption1">{eventName}</p>
-                <p className="text-body text-text-default font-semibold">{customerName}</p>
+              <p className="text-text-subtlest text-caption1">{eventName}</p>
+              <p className="text-body text-text-default font-semibold">
+                {customerName}
+              </p>
             </div>
             <div className="flex flex-col items-end">
-                <p className="text-caption1 text-text-subtlest">픽업 시간</p>
-                <p className="text-body text-text-default font-semibold">{pickupTime}</p>
+              <p className="text-caption1 text-text-subtlest">픽업 시간</p>
+              <p className="text-body text-text-default font-semibold">
+                {pickupTime}
+              </p>
             </div>
           </div>
         </div>
       </div>
       <div className="flex items-center gap-5">
         <div className="flex items-center gap-1">
-            <p className="text-caption1 text-text-subtlest">메뉴</p>
-            <p className="text-text-default text-caption1 font-semibold">
-                {menu}
-            </p>
+          <p className="text-caption1 text-text-subtlest">메뉴</p>
+          <p className="text-text-default text-caption1 font-semibold">
+            {menu}
+          </p>
         </div>
         <div className="flex items-center gap-1">
-            <p className="text-text-subtlest text-caption1">수량</p>
-            <p className="text-text-default text-caption1 font-semibold">{quantity}개</p>
+          <p className="text-text-subtlest text-caption1">수량</p>
+          <p className="text-text-default text-caption1 font-semibold">
+            {quantity}개
+          </p>
         </div>
       </div>
     </div>
