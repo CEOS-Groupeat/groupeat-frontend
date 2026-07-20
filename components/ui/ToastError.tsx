67db25e1fb71b5d837b12baf111752b59a0c5db4
a-00-a <sy.lee.2391@gmail.com>
2026-07-21 04:54:10 +0900

    Fix: 에러토스트 bottom props 추가

--- components/ui/ToastError.tsx
+++ components/ui/ToastError.tsx
@@ -2,13 +2,15 @@ import StatusError from '@/public/icons/icon_status_error.svg';
 
 interface ToastErrorProps {
   text: string;
+  bottom?: number;
 }
 
-export default function ToastError({ text }: ToastErrorProps) {
+export default function ToastError({ text, bottom = 80 }: ToastErrorProps) {
   return (
-    <div 
+    <div
+      style={{ bottom: `${bottom}px` }}
       className="flex w-fit pl-3 pr-4 py-1.5 justify-center items-center gap-1 rounded-full bg-background-toast/52 backdrop-blur-[32px] 
-      fixed bottom-20 left-1/2 -translate-x-1/2 z-toast animate-in fade-in slide-in-from-bottom-5 duration-300"
+      fixed left-1/2 -translate-x-1/2 z-toast animate-in fade-in slide-in-from-bottom-5 duration-300"
     >
       <StatusError className="shrink-0" />
       <p className="text-label1 text-text-inverse whitespace-nowrap">{text}</p>
